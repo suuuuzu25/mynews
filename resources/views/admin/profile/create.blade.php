1f0b41084c646c66082b69afdfca95965c56c4ae
Suzuka <suuuuzu77@gmail.com>
2020-02-28 02:27:17 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールの新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
